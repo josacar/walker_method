279fa11fe8dbf44991a06bd30b8cce02325d6aa6
Jose Luis Salas <josacar@users.noreply.github.com>
2020-10-29 22:28:40 +0100

    Add test and fix overflow when summing probabilities

--- spec/walker_method_spec.cr
+++ spec/walker_method_spec.cr
@@ -10,4 +10,20 @@ describe WalkerMethod do
     (samples["hello"] / samples["world"]).should be > 3.8
     (samples["hello"] / samples["world"]).should be < 4.2
   end
+
+  it "should work with two big arrays" do
+    words = ["you", "the", "i", "to", "a", "and", "it", "of", "that", "in", "is", "me", "what", "this", "for", "my", "on", "your", "we", "have", "do", "no", "don't", "are", "be"] * 500
+
+    freqs = [4621939, 3957465, 3476773, 2873389, 2551033, 1775393, 1693042, 1531878, 1323823, 1295198, 1242191, 1208959, 1071825, 961194, 898671, 877684, 867296, 834953, 819499, 812625, 799991, 788200, 764177, 743194, 743014] * 500
+
+    sampler = WalkerMethod.new(words, freqs)
+    samples = Hash(String, Float64).new(0.0)
+
+    10_000.times do
+      samples[sampler.random] += 1
+    end
+
+    (samples["you"] / samples["on"]).should be < 7
+    (samples["you"] / samples["on"]).should be > 2
+  end
 end
--- src/walker_method.cr
+++ src/walker_method.cr
@@ -7,15 +7,16 @@ class WalkerMethod
   def initialize(keys : Array(String), weights : Array(Int32))
     @keys = keys
     @weights = weights
-    @sumw = weights.sum.as(Int32)
+    @sumw = weights.sum(BigInt.new(0)).as(BigInt)
     @prob = [] of BigFloat
     @inx = [] of Int32
     @length = weights.size.as(Int32)
     short = [] of Int32
     long = [] of Int32
+
     weights.each do |w|
       inx << -1
-      prob << w * ( BigInt.new(length) / BigFloat.new(sumw) )
+      prob << w * ( BigInt.new(length) / sumw )
     end
 
     prob.each.with_index do |p, index|
@@ -28,6 +29,7 @@ class WalkerMethod
       k = long[-1]
       inx[j] = k
       prob[k] -= (1 - prob[j])
+
       if prob[k] < 1
         short << k
         long.pop
